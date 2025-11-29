48cedfd637f8ae8246ba57a2c2c9b36bd6999a04
piayer69 <maxmarsz1@gmail.com>
2025-11-29 17:16:32 +0100

    Little change to boilerplate data

--- frontend/app/components/article/ArticleFormating.tsx
+++ frontend/app/components/article/ArticleFormating.tsx
@@ -9,29 +9,29 @@ import styles from './ArticleFormating.module.css'
 const ArticleFormatting = () => {
     const [showInfo, setShowInfo] = useState(false);
 
-  return (
-    <div className={styles.formatingContainer}>
-        <div className={styles.formatingTitle} onClick={() => setShowInfo(!showInfo)}>
-            Formatowanie artykułu
-            <FontAwesomeIcon icon={faCircleQuestion} className={styles.infoIcon} />
+    return (
+        <div className={styles.formatingContainer}>
+            <div className={styles.formatingTitle} onClick={() => setShowInfo(!showInfo)}>
+                Formatowanie raportu
+                <FontAwesomeIcon icon={faCircleQuestion} className={styles.infoIcon} />
+            </div>
+            {showInfo &&
+                <div className={styles.formatingDescription}>
+                    <p>
+                        Artykuł powinien być napisany w języku angielskim, z użyciem stylu akademickiego.
+                        Poniżej znajdują się podstawowe wytyczne dotyczące formatowania:
+                    </p>
+                    <ul>
+                        <li>Użyj czcionki Times New Roman, rozmiar 12.</li>
+                        <li>Marginesy: 1 cal z każdej strony.</li>
+                        <li>Podwójne odstępy między wierszami.</li>
+                        <li>Strony powinny być ponumerowane.</li>
+                        <li>Bibliografia powinna być umieszczona na końcu artykułu.</li>
+                    </ul>
+                </div>
+            }
         </div>
-        {showInfo &&
-        <div className={styles.formatingDescription}>
-            <p>
-                Artykuł powinien być napisany w języku angielskim, z użyciem stylu akademickiego. 
-                Poniżej znajdują się podstawowe wytyczne dotyczące formatowania:
-            </p>
-            <ul>
-                <li>Użyj czcionki Times New Roman, rozmiar 12.</li>
-                <li>Marginesy: 1 cal z każdej strony.</li>
-                <li>Podwójne odstępy między wierszami.</li>
-                <li>Strony powinny być ponumerowane.</li>
-                <li>Bibliografia powinna być umieszczona na końcu artykułu.</li>
-            </ul>
-        </div>
-        }
-    </div>
-  )
+    )
 }
 
 export default ArticleFormatting
